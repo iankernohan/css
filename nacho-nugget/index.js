489e1f042735d6b630a14a9512d844ca780c736c
Ian Kernohan <kernohani1@gmail.com>
2026-08-06 18:13:44 -0400

    add zoom functionality

--- nacho-nugget/index.js
+++ nacho-nugget/index.js
@@ -2,23 +2,40 @@ const left = document.getElementById("button-left");
 const right = document.getElementById("button-right");
 const up = document.getElementById("button-up");
 const down = document.getElementById("button-down");
-const scene = document.querySelector(".scene");
+const zoomIn = document.getElementById("button-zoom-in");
+const zoomOut = document.getElementById("button-zoom-out");
+const zoomRange = document.getElementById("zoom-range");
 const xDisplay = document.getElementById("x-display");
 const yDisplay = document.getElementById("z-display");
+const scene = document.querySelector(".scene");
 
 let initialZ = 45;
 let initialX = 65;
+let initialFontSize = 20;
 let intervalId;
 const fireRate = 100;
 const turnAmount = 5;
+const fontSizeChangeAmount = 1;
 
 let currentX = localStorage.getItem("currentX");
 let currentZ = localStorage.getItem("currentZ");
-if (!currentX) localStorage.setItem("currentX", initialX);
-if (!currentZ) localStorage.setItem("currentZ", initialZ);
+let currentFontSize = parseInt(localStorage.getItem("currentFontSize"));
+if (!currentX) {
+  localStorage.setItem("currentX", initialX);
+  currentX = initialX;
+}
+if (!currentZ) {
+  localStorage.setItem("currentZ", initialZ);
+  currentZ = initialZ;
+}
+if (!currentFontSize) {
+  localStorage.setItem("currentFontSize", initialFontSize);
+  currentFontSize = initialFontSize;
+}
 
+document.body.style.fontSize = `${currentFontSize}px`;
+zoomRange.value = currentFontSize + "";
 scene.style.transform = `rotateX(${currentX ?? initialX}deg) rotateZ(${currentZ ?? initialZ}deg)`;
-
 updateDisplay(currentX ?? initialX, currentZ ?? initialZ);
 
 function updateDisplay(x, z) {
@@ -126,3 +143,28 @@ function turndown() {
   scene.style.transform = `rotateX(${updated}deg) rotateZ(${currentZ}deg)`;
   updateDisplay(updated, currentZ);
 }
+
+zoomIn.addEventListener("click", handleZoomIn);
+
+function handleZoomIn() {
+  const updated = currentFontSize + fontSizeChangeAmount;
+  console.log(updated);
+  document.body.style.fontSize = `${updated}px`;
+  const zoomAmount = localStorage.setItem("currentFontSize", updated);
+  currentFontSize = updated;
+}
+
+zoomOut.addEventListener("click", handleZoomOut);
+
+function handleZoomOut() {
+  const updated = currentFontSize - fontSizeChangeAmount;
+  document.body.style.fontSize = `${updated}px`;
+  const zoomAmount = localStorage.setItem("currentFontSize", updated);
+  currentFontSize = updated;
+}
+
+zoomRange.addEventListener("input", (e) => {
+  document.body.style.fontSize = `${e.target.value}px`;
+  const zoomAmount = localStorage.setItem("currentFontSize", e.target.value);
+  currentFontSize = parseInt(e.target.value);
+});
